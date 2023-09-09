d0250433215828afc4f2ff74d5896e82292c5450
Vital <Vitalii8881878@gmail.com>
2023-09-09 08:01:42 -0400

    solution 8kyu else if

---
+++ 8kyu/8kyuMultiply.js
@@ -0,0 +1,10 @@
+/*8 kyu Multiply
+Этот код не выполняется должным образом. Попытайтесь выяснить, почему.
+This code does not execute properly. Try to figure out why.
+multiply = function (a, b) {
+     a * b;
+}
+*/
+multiply = function (a, b) {
+    return a * b;
+}
