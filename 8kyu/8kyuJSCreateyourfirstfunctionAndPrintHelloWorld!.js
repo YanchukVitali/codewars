22a04b9971db851604f02ed53bf4c8f8dc85c782
Vital <Vitalii8881878@gmail.com>
2023-09-10 19:29:17 -0400

    solution8kyu- create JS function and print

---
+++ 8kyu/8kyuJSCreateyourfirstfunctionAndPrintHelloWorld!.js
@@ -0,0 +1,14 @@
+/*n JavaScript, your code is running in a function, let us step by step complete your first JS function.
+Look at this example:
+      --------keyword "function"
+      |       ----your function name
+      |       |    ---if needed, some arguments will appear in parentheses
+      |       |    |
+    function myfunc(){  ---------your function code will starting with "{"
+      //you should type your code here
+    }----------------------------ending with "}"
+ */
+helloWorld = function() {
+    let str = "Hello World!";
+    console.log( str );
+}
