53af76eb157aa28efe526b25f9fc373b42448097
Vital <Vitalii8881878@gmail.com>
2023-12-12 21:33:05 -0500

    solution-8kyuAlanPartridgeII-AppleTurnover.js

---
+++ 8kyu/8kyuAlanPartridgeII-AppleTurnover.js
@@ -0,0 +1,18 @@
+/*function apple(x) {
+}
+If x >1000 return "It's hotter than the sun!!"
+else "Help yourself to a honeycomb Yorkie for the glovebox."*/
+
+
+function apple(x){
+    x = x*x
+    if (x > 1000) {
+        return  "It's hotter than the sun!!";
+    } else {
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
+    }
+}
+
+
+
+
